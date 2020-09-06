cb3c8ddd2f4406e7e9b84163eac9ed54e9f0e2f2
0bmxa <0bmxa@users.noreply.github.com>
2020-09-06 19:06:46 +0200

    Optimize documentation

--- Sources/Synchronizer/Synchronizer.swift
+++ Sources/Synchronizer/Synchronizer.swift
@@ -17,13 +17,17 @@ import Foundation.NSDate
 public class Synchronizer<Response> {
     /// The counting semaphore doing all the magic.
     private let semaphore = DispatchSemaphore(value: 0)
-    /// The internal storage for the value to be transported.
+    /// The internal storage for the value to be captured.
     private var _value: Response!
 
     /// Creates a new non-cancellable Synchronizer object.
     public init() {}
 
-    /// The value to be transported.
+    /// The value to be captured.
+    /// - Important:
+    ///   - Read access to this is blocking (sends the thread to sleep).
+    ///   - Write access triggers continuation of the previously paused
+    ///     execution (wakes the thread up).
     public var value: Response {
         set {
             self._value = newValue
@@ -43,19 +47,19 @@ public class Synchronizer<Response> {
 public class CancellableSynchronizer<Response> {
     /// The counting semaphore doing all the magic.
     private let semaphore = DispatchSemaphore(value: 0)
-    /// The internal storage for the value to be transported.
+    /// The internal storage for the value to be captured.
     private var _value: Response?
     /// The timeout in seconds, if any.
     private let timeout: TimeInterval?
     
     /// Creates a new CancellableSynchronizer object.
     /// - Parameter timeout: An optional timeout value (in seconds) of how long
-    ///   to wait before cancelling the blocking
+    ///   to wait before cancelling the blocking.
     public init(timeout: TimeInterval? = nil) {
         self.timeout = timeout
     }
     
-    /// The value to be transported.
+    /// The value to be captured.
     /// - Important:
     ///   - Read access to this is blocking (sends the thread to sleep). It
     ///     starts the timeout countdown, if set.
@@ -77,7 +81,7 @@ public class CancellableSynchronizer<Response> {
         }
     }
     
-    /// Cancels the blocking, i.e. triggers continuation of the previously
+    /// Cancels the waiting, i.e. triggers continuation of the previously
     /// paused execution.
     public func cancel() {
         self.semaphore.signal()
