8cbc50e6729522e10197a4d25309c1ed591f0a14
0bmxa <0bmxa@users.noreply.github.com>
2020-09-06 19:18:02 +0200

    Add tests for CancellableSynchronizer

--- Tests/SynchronizerTests/SynchronizerTests.swift
+++ Tests/SynchronizerTests/SynchronizerTests.swift
@@ -11,19 +11,47 @@ func myAsyncFunction(greet: String, whom: String, callback: @escaping (String) -
 
 
 final class SynchronizerTests: XCTestCase {
-    func testNotWorking() {
+    /// Tests that `myAsyncFunction` actually behaves async.
+    func testAsyncFailure() {
         var output = ""
         myAsyncFunction(greet: "Hello", whom: "World") { message in
             output = message
         }
         XCTAssertNotEqual(output, "Hello World!")
     }
-
-    func testWorking() {
+    
+    /// Tests Synchronizer.
+    func testSynchronizer() {
         let sync = Synchronizer<String>()
         myAsyncFunction(greet: "Hello", whom: "World") { message in
             sync.value = message
         }
         XCTAssertEqual(sync.value, "Hello World!")
+    }
+
+    /// Tests CancellableSynchronizer.
+    func testCancellableSynchronizer() {
+        let sync = CancellableSynchronizer<String>()
+        myAsyncFunction(greet: "Hello", whom: "World") { message in
+            sync.value = message
+        }
+        let response = sync.value
+        XCTAssertNotNil(response)
+        XCTAssertEqual(response!, "Hello World!")
+    }
+
+    /// Tests CancellableSynchronizer can be cancelled.
+    func testCancellableSynchronizerCanBeCancelled() {
+        let sync = CancellableSynchronizer<String>()
+        myAsyncFunction(greet: "Hello", whom: "World") { message in
+            sync.value = message
+        }
+        
+        DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + 0.5) {
+            sync.cancel()
+        }
+
+        let response = sync.value
+        XCTAssertNil(response)
     }
 }
